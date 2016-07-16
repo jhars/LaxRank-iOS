25aed4bacbe3ce6c5090fde18858551bb795cb11
Jonathan Harlan <jjharlan06@gmail.com>
2016-07-15 23:52:22 -0400

    Super Basic VC Navigation

--- LaxRank02/ViewController.swift
+++ LaxRank02/ViewController.swift
@@ -12,7 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        buildNavBar()
+        
+    }
+    
+    func buildNavBar() {
+        let frame = view.frame.size
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: frame.width, height: 100))
+        let navItem = UINavigationItem(title: "LaxRank");
+        
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewCtlr = storyBoard.instantiateViewControllerWithIdentifier("teamTemplate")
+        
+        let browseItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Search, target: nextViewCtlr, action: Selector("browseTeamsBtnClicked"));
+        
+        navItem.rightBarButtonItem = browseItem;
+        navBar.setItems([navItem], animated: false);
+        
+        self.view.addSubview(navBar);
+    }
+    
+    func browseTeamsBtnClicked() {
+//        self.performSegueWithIdentifier("NatTop25", sender: nil)
+//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+//        let nextViewCtlr = storyBoard.instantiateViewControllerWithIdentifier("teamTemplate")
+//        let nextViewController = storyBoard.instantiateViewControllerWithIdentifier("nextView") as NextViewController
+//        self.presentViewController(nextViewCtlr, animated:true, completion:nil)
+//        self.presentViewController(nextViewCtlr, animated:true, completion: nil)
+        
+        self.performSegueWithIdentifier("browseAll", sender: self)
     }
 
     override func didReceiveMemoryWarning() {
